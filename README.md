581386fb723ca1eb26bf7795205bcb5405475191
Genki Mine <gomfucius@gmail.com>
2021-09-22 11:46:45 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # MyLibrary
 
-A description of this package.
+This is a sample project of the question I've asked here:
+
+https://stackoverflow.com/questions/69288425/how-to-build-and-test-an-ios-swift-package-officially
+
+FloatingPanel.xcframework is created using https://github.com/scenee/FloatingPanel, this is just to illustrate a problem I faced using a local xcframework in a Swift Package.
--- Sources/MyLibrary/MyLibrary.swift
+++ Sources/MyLibrary/MyLibrary.swift
@@ -1,3 +1,20 @@
+import FloatingPanel
+import RxSwift
+import RxRelay
+
 struct MyLibrary {
     var text = "Hello, World!"
+    var relay = BehaviorRelay(value: 1)
+    var disposable = DisposeBag()
+    
+    func makeFloatingPanel() {
+        let panel = FloatingPanelController()
+        print(panel) // Verify that this compiles
+    }
+    
+    func doRx() {
+        relay.subscribe { value in
+            print(value)
+        }.disposed(by: disposable)
+    }
 }
